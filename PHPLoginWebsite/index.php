edcba6f2e8e72eec7da67a72c43fccf4e166a840
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2018-01-10 00:39:15 +0000

    Template before any PHP is added

---
+++ PHPLoginWebsite/index.php
@@ -0,0 +1,82 @@
+<?php 
+    require("config.php"); 
+    $submitted_username = ''; 
+    if(!empty($_POST)){ 
+        $query = "CALL selectUsername(?)"; 
+        
+        try{ 
+            $stmt = $db->prepare($query); 
+            $stmt->bindParam(1, $_POST['username'] , PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
+            $result = $stmt->execute(); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+        $login_ok = false; 
+        $row = $stmt->fetch(); 
+        if($row){ 
+            $check_password = hash('sha256', $_POST['password'] . $row['salt']); 
+            for($round = 0; $round < 65536; $round++){
+                $check_password = hash('sha256', $check_password . $row['salt']);
+            } 
+            if($check_password === $row['password']){
+                $login_ok = true;
+            } 
+        } 
+
+        if($login_ok){ 
+            unset($row['salt']); 
+            unset($row['password']); 
+            $_SESSION['user'] = $row;  
+            header("Location: secret.php"); 
+            die("Redirecting to: secret.php"); 
+        } 
+        else{ 
+            print("Login Failed."); 
+            $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8'); 
+        } 
+    } 
+?> 
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+      
+      <link href="css/signin.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    
+      
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ PHPLoginWebsite/test3/q3no.php
@@ -0,0 +1,30 @@
+<?php
+
+// Only process the form if $_POST isn't empty
+if ( ! empty( $_POST ) ) {
+    
+    // Connect to MySQL
+    $mysqli = new mysqli( 'localhost', 'pugsleyn', '!OWVmNjQzMmVhNzA2Zjk3ODMxMDA1NWY3', 'DB1pugsleyn' );
+    
+    // Check the connection
+    if ( $mysqli->connect_error) {
+        die( 'Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error );
+    }
+    
+    // Insert the data
+    $sql = "UPDATE question SET no = no + 1 WHERE Qid = 3";
+    $insert = $mysqli->query($sql);    
+     
+    // Print response from MySQL
+    if ( $insert ) {
+        echo "Success! Row ID: {$mysqli->insert_id}";
+    } else {
+        die("Error: {$mysqli->errno} : {$mysqli->error}");
+    }
+    
+    // Close the connection
+    $mysqli->close();
+}
+    // Redirects to previous page
+    header("location: javascript://history.go(-1)");
+?>
